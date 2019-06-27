a53dc413000e6aa541c49e3df674d06a9f01a20b
kassanj <kassandram022@gmail.com>
2019-06-27 11:16:58 -0700

    add state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Map from './Map.js';
 
-function App() {
-  return (
-    <div className="App">
-        <Map />
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      latitude: 40.7128,
+      longitude: -74.006,
+      style: 'mapbox://styles/mapbox/dark-v9',
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+          <Map app={this} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/Map.js
+++ src/Map.js
@@ -5,12 +5,23 @@ import './Map.css';
 class Map extends Component {
 
   componentDidMount() {
+    const app = this.props.app
+
     mapbox.accessToken = 'pk.eyJ1Ijoia2Fzc2FuZHJhbSIsImEiOiJjajRkMHBxOWEwZ2YxMzJydHV6bjdydHVvIn0.B1BbwVvXXRVei9FFgANoAA';
 
-    var map = new mapbox.Map({
+    const map = new mapbox.Map({
       container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v11'
+      style: app.state.style,
+      center: [
+        app.state.longitude,
+        app.state.latitude
+      ],
+      zoom: 12
     });
+
+    const navigationControl = new mapbox.NavigationControl();
+    map.addControl(navigationControl);
+
   }
 
   render() {
