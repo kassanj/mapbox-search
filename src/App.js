c9720d232c08520cc7e87316e5644a8bf0ad9e03
kassanj <kassandram022@gmail.com>
2019-07-01 11:13:25 -0700

    create places panel and place item components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Map from './Map.js';
 import Toggler from './Toggler.js';
 import Search from './Search.js';
+import PlacesPanel from './PlacesPanel.js';
 
 class App extends Component {
 
@@ -13,12 +14,20 @@ class App extends Component {
       latitude: 40.7128,
       longitude: -74.006,
       style: 'mapbox://styles/mapbox/dark-v9',
+      places: [
+        { name: "13 Crosby Street", latitude: 40.7128, longitude: -74.006 },
+        { name: "14 Crosby Street", latitude: 40.7128, longitude: -74.006 },
+        { name: "15 Crosby Street", latitude: 40.7128, longitude: -74.006 },
+        { name: "16 Crosby Street", latitude: 40.7128, longitude: -74.006 },
+        { name: "17 Crosby Street", latitude: 40.7128, longitude: -74.006 }
+      ]
     }
   }
 
   render() {
     return (
       <div className="App">
+          <PlacesPanel app={this} />
           <Search app={this} />
           <Toggler app={this} />
           <Map app={this} />
